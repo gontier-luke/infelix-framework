b989e2086432f1d352975448ccfed963afa074e3
Infelix Commentator <lukegontier13@gmail.com>
2026-04-15 12:28:56 +0200

    - amélioration de la page d'initialisation

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php 
 
+use classes\Router;
+use Repositories\Configuration;
+
 session_start();
 
 require_once 'var/base_func.php';
 
+foreach(glob('./enum/*.php') as $fileName){
+
+    if (!str_contains($fileName,'index.php')) {
+        require_once $fileName;
+    }
+}
 
 foreach(glob('./classes/*.php') as $fileName){
     if (!str_contains($fileName,'index.php')) {
@@ -17,6 +26,11 @@ foreach(glob('./classes/**/*.php') as $fileName){
     }
 }
 
+foreach(glob('./overrides/*.php') as $fileName){
+    if (!str_contains($fileName,'index.php')) {
+        require_once $fileName;
+    }
+}
 
 foreach(glob('./controllers/*.php') as $fileName){
     if (!str_contains($fileName,'index.php')) {
@@ -31,18 +45,42 @@ foreach(glob('./models/*.php') as $fileName){
     }
 }
 
-
 foreach(glob('./repositories/*.php') as $fileName){
 
     if (!str_contains($fileName,'index.php')) {
         require_once $fileName;
     }
 }
+
+foreach(glob('./entities/*.php') as $fileName){
+
+    if (!str_contains($fileName,'index.php')) {
+        require_once $fileName;
+    }
+}
+
+foreach(glob('./services/*.php') as $fileName){
+
+    if (!str_contains($fileName,'index.php')) {
+        require_once $fileName;
+    }
+}
+
+$router = new Router();
+
+$activeModules = getAllActiveModules();
+
+foreach($activeModules as $module) {
+    $modulePath = './modules/' . $module->getName() . '/init.php';
+    if(file_exists($modulePath)) {
+        require_once $modulePath;
+    }
+}
+
 if(Configuration::get('ssl') === '1') {
     if($_SERVER['REQUEST_SCHEME'] === 'http') {
         header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
         exit;
     }
 }
-$router = new Router();
 $router->handleRequest($_GET['path'] ?? '/');
